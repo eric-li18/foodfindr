0f578a7f39631a64143fdb054081460f24c58c4c
Eric <erickh.li@mail.utoronto.ca>
2020-04-03 21:05:47 -0400

    Added to README.md

--- foodfindr/lib/main.dart
+++ foodfindr/lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'foodfindr',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -18,9 +17,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'foodfindr'),
     );
   }
 }
