ccf6ac37e22fcea64d3d9afff4c45416ba893f8d
tt-le <tienthanh.le@mail.utoronto.ca>
2020-04-13 21:27:28 -0400

    Added favourites, history and profile pages

---
+++ lib/screens/profile/profile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:foodfindr/screens/home/navigation.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text('favourites page')),
+      bottomNavigationBar: Navigation(),
+    );
+  }
+}
