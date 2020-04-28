0a2f292d328cce6c27cfb21e7e083df618364388
eric-li18 <eric.kh.li0821@gmail.com>
2020-04-27 23:59:32 -0400

    added code to requests.dart

--- lib/services/requests.dart
+++ lib/services/requests.dart
@@ -1,24 +1,44 @@
 import 'package:http/http.dart' as http;
 import 'package:geolocator/geolocator.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 
 class Requests{
-  Position _currentPosition;
+  final RemoteConfig _remoteConfig = await RemoteConfig.instance;
+  await _remoteConfig.fetch(expiration: const Duration(hours: 1));
+  await _remoteConfig.activateFetched();
 
-  _getCurrentLocation() {
-      final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+  _remoteConfig.getValue('YELP_API_KEY').asString();
+  // Position _currentPosition;
 
-      geolocator
-          .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-          .then((Position position) {
-        setState(() {
-          _currentPosition = position;
-        });
-      }).catchError((e) {
-        print(e);
-      });
-    }
+  // _getCurrentLocation() {
+  //     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
+  //     geolocator
+  //         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+  //         .then((Position position) {
+  //       setState(() {
+  //         _currentPosition = position;
+  //       });
+  //     }).catchError((e) {
+  //       print(e);
+  //     });
+  //   }
 
+  // _getAddressFromLatLng() async {
+  //     try {
+  //       List<Placemark> p = await geolocator.placemarkFromCoordinates(
+  //           _currentPosition.latitude, _currentPosition.longitude);
+
+  //       Placemark place = p[0];
+
+  //       setState(() {
+  //         _currentAddress =
+  //             "${place.locality}, ${place.postalCode}, ${place.country}";
+  //       });
+  //     } catch (e) {
+  //       print(e);
+  //     }
+  //   }
 
 
 }
