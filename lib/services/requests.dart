f82c5577d7c0631751884e4405d40960a3dbf9fd
eric-li18 <eric.kh.li0821@gmail.com>
2020-04-28 01:05:32 -0400

    Worked on Yelp API calls

--- lib/services/requests.dart
+++ lib/services/requests.dart
@@ -1,44 +1,55 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:geolocator/geolocator.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 
-class Requests{
-  final RemoteConfig _remoteConfig = await RemoteConfig.instance;
-  await _remoteConfig.fetch(expiration: const Duration(hours: 1));
-  await _remoteConfig.activateFetched();
+class Requests {
+  String _key;
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+  Position _currentPosition;
+  String _currentAddress;
 
-  _remoteConfig.getValue('YELP_API_KEY').asString();
-  // Position _currentPosition;
+  Requests() {
+    _setKey();
+  }
 
-  // _getCurrentLocation() {
-  //     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+  _setKey() async {
+    RemoteConfig _remoteConfig = await RemoteConfig.instance;
+    await _remoteConfig.fetch(expiration: const Duration(hours: 1));
+    await _remoteConfig.activateFetched();
 
-  //     geolocator
-  //         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-  //         .then((Position position) {
-  //       setState(() {
-  //         _currentPosition = position;
-  //       });
-  //     }).catchError((e) {
-  //       print(e);
-  //     });
-  //   }
+    _key = _remoteConfig.getValue('YELP_API_KEY').asString();
+  }
 
-  // _getAddressFromLatLng() async {
-  //     try {
-  //       List<Placemark> p = await geolocator.placemarkFromCoordinates(
-  //           _currentPosition.latitude, _currentPosition.longitude);
+  _getCurrentLocation() {
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
-  //       Placemark place = p[0];
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      _currentPosition = position;
+    }).catchError((e) {
+      print(e);
+    });
+  }
 
-  //       setState(() {
-  //         _currentAddress =
-  //             "${place.locality}, ${place.postalCode}, ${place.country}";
-  //       });
-  //     } catch (e) {
-  //       print(e);
-  //     }
-  //   }
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> p = await geolocator.placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
 
+      Placemark place = p[0];
 
+      _currentAddress =
+          "${place.locality}, ${place.postalCode}, ${place.country}";
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<Map<String,dynamic>> getRestaurantsNearby() async {
+    var response = await http.get("https://api.yelp.com/v3/businesses/search?latitude=" + _currentPosition.latitude.toString() + "&longitude=" + _currentPosition.longitude.toString() + "&radius=5000&limit=5",headers: {"Authorization": "Bearer " + _key});
+    return json.decode(response.body);
+  }
 }
