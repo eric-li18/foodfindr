0507a93c43fb8968fe01e0999d060e5c56de41fa
tt-le <tienthanh.le@mail.utoronto.ca>
2020-05-03 01:08:21 -0400

    history cards

---
+++ lib/screens/history/history_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HistoryCard extends StatefulWidget {
+  @override
+  _HistoryCardState createState() => _HistoryCardState();
+}
+
+class _HistoryCardState extends State<HistoryCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+                // color: Colors.white,
+                padding: EdgeInsets.only(left: 15, bottom: 15, top: 30),
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusDirectional.only(
+                      topStart: Radius.circular(30),
+                      topEnd: Radius.circular(30)
+                    )
+                  )
+                ),
+                child: Text(
+                  'What\'s nearby',
+                  style: TextStyle(
+                    fontFamily: 'Futura Medium',
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+    );
+  }
+}
